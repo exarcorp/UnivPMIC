dd312e12b5c773d3145a6740c6a67d10dd6dd6bc
Exar Corporation <powertechsupport@exar.com>
2017-03-30 09:37:48 -0700

    UnivPMIC

---
+++ examples/Xrp7724_Clear_Erase_Multiple/Xrp7724_Clear_Erase_Multiple.ino
@@ -0,0 +1,47 @@
+#include <Wire.h>
+#include "PowerXR.h"
+
+Xrp7724 chip(0x28); //assuming i2c_addr is 0x28
+int ret_val = -1;
+char ch = 0;
+
+void setup()
+{  
+  Wire.begin();
+  Serial.begin(9600);
+  Serial.println("Press 'x' to begin clear and erase cycles....");
+}
+  
+void loop()
+{
+  delay(1000);
+  ch = Serial.read(); // waiting for keyboard input 'x' to begin clear and erase cycles
+  if(ch=='x'){
+    for(int i=0; i<=6; i++){
+      Serial.println("-------------------------");
+      Serial.print("Clearing Page: ");
+      Serial.println(i);
+      Serial.println("-------------------------");
+      ret_val = chip.clear_flash_page(i); // function based on ANP-38
+      delay(50);
+      if(ret_val == 0)
+        Serial.println(" Page Successfully Cleared ");
+      
+      Serial.println("-------------------------");
+      Serial.print("Erasing Page: ");
+      Serial.println(i);
+      Serial.println("-------------------------");
+      for(int j=0; j<4; j++){ //num of cycles for erase
+        Serial.print(" Cycle: ");
+        Serial.println(j+1);
+        ret_val = chip.erase_flash_page(i); // function based on ANP-38
+        delay(50);
+        if(ret_val == 0)
+          Serial.println(" Page Successfully Erased ");
+      }    
+    }
+    
+    Serial.println("------- Finished Clear and Erase---------");
+    delay(500);
+  }
+}
---
+++ examples/voltage_scaling_with_zeb_7724/voltage_scaling_with_zeb_7724.ino
@@ -0,0 +1,43 @@
+/*
+ * Voltage scaling with zero error bin example.
+ */
+ 
+#include <Wire.h>
+#include "PowerXR.h"
+
+#define ENABLE 1
+#define DISABLE 0
+
+byte ch_num = 0; // 0 = channel 1
+float set_val = 3.1698; // 3.1698 3.1611 set voltage to 3.0V
+
+Xrp7724 chip(0x28);
+
+
+void setup()
+{  
+  Wire.begin();
+  Serial.begin(9600);
+  chip.power_enable_supply(ch_num, DISABLE);
+  delay(500);
+  chip.power_enable_supply(ch_num, ENABLE);
+  delay(2000);
+  chip.standard_write(0x20, 16+152);
+  delay(500);
+}
+
+
+void loop()
+{
+
+  delay(5000);
+  // setting voltage for channel 1 to set_val
+//  chip.power_set_voltage_ch(ch_num, set_val);
+  chip.standard_write(0x20, 48+200);
+//  chip.power_set_voltage_native_fine_adj_ch(ch_num, set_val);
+  Serial.print("Voltage Set to : ");
+  Serial.print(set_val);
+  Serial.println(" V");
+//  Serial.println(chip.get_fault_status());
+}
+
